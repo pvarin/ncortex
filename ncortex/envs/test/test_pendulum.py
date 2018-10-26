c4ce122de5f0396f86377778cea0b761d2348d1e
Patrick Varin <patrickjvarin@gmail.com>
2018-10-25 21:21:19 -0400

    adding tests for the Pendulum final state

--- ncortex/envs/test/test_pendulum.py
+++ ncortex/envs/test/test_pendulum.py
@@ -15,8 +15,9 @@ class TestPendulum(tf.test.TestCase):
         '''
         self.x0 = tf.constant([1., 2.])
         self.Q = 3. * np.eye(2, dtype=np.float32)
-        self.R = 4. * np.eye(1, dtype=np.float32)
-        self.pend = Pendulum(x0=self.x0, Q=self.Q, R=self.R)
+        self.Q_f = 4. * np.eye(2, dtype=np.float32)
+        self.R = 5. * np.eye(1, dtype=np.float32)
+        self.pend = Pendulum(x0=self.x0, Q=self.Q, Q_f=self.Q_f, R=self.R)
         self.pend.reset()
 
     def test_transition_cost(self):
@@ -55,6 +56,37 @@ class TestPendulum(tf.test.TestCase):
             grad = tf.gradients(cost, [state, action])
             self.assertAllClose(grad[0], 2. * self.Q[0, 0] * state)
             self.assertAllClose(grad[1], 2. * self.R[0, 0] * action)
+
+    def test_final_cost(self):
+        ''' Test the final cost method.
+        '''
+        with self.cached_session():
+            # Compute the transition cost with an arbitrary state and action.
+            state = tf.constant([1., 2.])
+            cost = self.pend.final_cost(state)
+
+            # Test the value.
+            self.assertAllClose(cost, self.Q_f[0, 0] * (1. + 4.))
+
+            # Test the gradients.
+            grad = tf.gradients(cost, [state])
+            self.assertAllClose(grad[0], 2. * self.Q_f[0, 0] * state)
+
+    def test_vectorized_final_cost(self):
+        ''' Test the final cost method with a vectorized state.
+        '''
+        with self.cached_session():
+            # Compute the final cost at an arbitrary state.
+            state = tf.constant([[1., 2.], [3., 4.], [5., 6.]])
+            cost = self.pend.final_cost(state)
+
+            # Test the value.
+            self.assertAllClose(cost,
+                                self.Q_f[0, 0] * tf.constant([5., 25., 61.]))
+
+            # Test the gradients.
+            grad = tf.gradients(cost, [state])
+            self.assertAllClose(grad[0], 2. * self.Q_f[0, 0] * state)
 
     def test_dynamics(self):
         ''' Test the dynamics method.
